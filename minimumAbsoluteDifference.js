c3c96b64a9d33561a7f32ef90c2d862627e3e78d
AbdulFattaah Popoola <abdulapopoola@gmail.com>
2018-02-20 21:13:42 -0800

    Update minimumAbsoluteDifference.js

--- minimumAbsoluteDifference.js
+++ minimumAbsoluteDifference.js
@@ -2,10 +2,7 @@ function minimumAbsoluteDifference(n, arr) {
     let sorted = arr.sort((a,b) => a - b);
     let minDiff = Infinity;
     for(let i = 1; i < n; i++){
-        let diff = Math.abs(arr[i] - arr[i-1]);
-        if(diff < minDiff) {
-            minDiff = diff
-        }
+        minDiff = Math.min(minDiff, Math.abs(arr[i] - arr[i-1]));
     }
     return minDiff;
 }
