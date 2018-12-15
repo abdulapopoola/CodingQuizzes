144140f78541cb7192317cf72603ac570a2d3f7f
AbdulFattaah Popoola <abdulapopoola@gmail.com>
2018-12-14 18:31:18 -0800

    Update fraudulentActivity.js

--- fraudulentActivity.js
+++ fraudulentActivity.js
@@ -23,6 +23,10 @@ function median(arr, maxValue) {
     }
 
     if(isEven){
+        let nextIndex = medianIndex + 1;
+        let nextSum = sum + countSortArr[nextIndex];
+        let nextMidpoint = midpoint + 1;
+
         sum = 0;
         let upperMedianIndex = 0;
         while(upperMedianIndex < len + 1) {
@@ -33,7 +37,24 @@ function median(arr, maxValue) {
             upperMedianIndex++;
         }
         return (medianIndex + upperMedianIndex) / 2;
-    }
+    } 
 
     return medianIndex;
 }
+
+// pass in countSorted array
+// change the values by using a n-size queue
+// pop and decrease count in countSorted array
+// push latest and increase count in countSorted
+// compare median * 2 with new value
+
+function assert(actual, expected) {
+    if(actual !== expected) {
+        throw 'Assert failed!';
+    }
+    console.log('Success!', actual, expected);
+}
+
+assert(median([1,3,3,6,6,7,8,9], 9), 6);
+assert(median([4,1,6,3,9,3,7,1,2,1,1,1,1,1], 9), 1.5);
+assert(median([3,3,5,6,7,8], 9), 5.5);
